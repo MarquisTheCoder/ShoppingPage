7cb98e3f6ef4934ae0e1c865aa0de7efdd69f4a4
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-03-17 16:39:54 -0500

    options

--- js/searchspringApi.js
+++ js/searchspringApi.js
@@ -6,24 +6,37 @@ class SearchSpringAPI {
   //results format should remain consistent through the calling
   //process but it can change this is also the reason why made it an
   //preset constructor parameter and not a method parameter
-  constructor(siteId = "scmq7n", resultsFormat = "native") {
+
+  constructor(siteId = "scmq7n", resultsFormat = "json") {
     this.siteId = siteId;
-    this.resultsFormat = "native";
+    this.resultsFormat = resultsFormat;
     this.baseUrl = `https://${siteId}.a.searchspring.io/api/search/search.json`;
   }
 
-  get siteId() {
+  //static properties
+  static #resultsPerPageDefault = 24;
+  static #options = { method: "GET", headers: { accept: "application/json" } };
+
+  // getters
+  static get siteId() {
     return this.siteId;
   }
 
-  get baseUrl() {
+  static get baseUrl() {
     return this.baseUrl;
+  }
+  // setters
+  static set siteId(id) {
+    this.siteId = id;
+  }
+
+  static set baseUrl(url) {
+    this.baseUrl = url;
   }
 
   //making the REST url creation seamless and easy for mulitple different request
-  static buildUrl() {
-    let url = this.#baseUrl;
-
+  static buildUrl(query, page, resultsPerPage = this.#resultsPerPageDefault) {
+    let url = `${this.baseUrl}?resultsFormat=${this.resultsFormat}&redirectResponse=minimal&page=${page}&resultsPerPage=${resultsPerPage}&q=${query}`;
     return url;
   }
 }
