54787c3eb16e1ed6416886faeae89e7ce7cb040b
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-03-19 15:32:20 -0500

    autosave script

--- js/globalStateManager.js
+++ js/globalStateManager.js
@@ -1,18 +1,18 @@
 
 const spring = new SearchSpringAPI();
 
-let state = {
-    currentUrl: spring.buildUrl("", page=1)
-}
 
 class GlobalStateManager{
 
+     static state = {
+            currentUrl: spring.buildUrl("", page=1)
+        }
     //using an intial json template to add all object keys 
     //and values and local store variables. provides me with a quick method
     //to start a new global state from scratch or potentially use this class elsewhere
     constructor(template){
         this.template = template;
-        this.init(template);
+        this.init(template); 
     }
     init(template){
         for(const [key, value] of Object.entries(state)){
