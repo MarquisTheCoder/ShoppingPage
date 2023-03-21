013f22dae55b2c2037fc0825a856e31505a1ec31
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-03-20 21:58:12 -0500

    working on the search filters

--- js/searching.js
+++ js/searching.js
@@ -3,40 +3,56 @@
 const searchInput = document.getElementById("search-input");
 let filterSettings
 
-function createFilter(name){
+
+function createFilter(name, type){
     return `
-     <div class="filter-item">
+     <div class="filter-item" data-type="${type}">
         <p>${name}</p>
      </div>
     `
 }
 
-function loadFilter(element, name){
-    element.innerHTML += createFilter(name);
+function loadFilter(element, name, type){
+    element.innerHTML += createFilter(name, type);
 }
+
+
 function loadFilters(){
     const filters =  {
         colors:["yellow", "amber", "orange", "vermillion", "red", "purple", "blue", "green"],
-        prices: ["small", "medium", "large"],
+        prices: ["low","lower","medium","high", "higher"],
+        sizes: ["small", "medium", "large"]
     };
 
     //using variables instead of accessing class array for code readability and semantics
     const colorFilters = document.getElementById("filter-item-colors");
     const pricingFilters = document.getElementById("filter-item-pricing");
-    const sizeFilters = document.getElementById("filter-item");
+    const sizeFilters = document.getElementById("filter-item-sizes");
 
     filters.colors.forEach(color => {
-       loadFilter(colorFilters, color); 
+       loadFilter(colorFilters, color, "color"); 
     }); 
     filters.prices.forEach(price => {
-        loadFilter(pricingFilters, price);
+        loadFilter(pricingFilters, price, "price");
     })
-    filters.sizeFilters.forEach(size => {
-        loadFilter(sizeFilters, size);
+    filters.sizes.forEach(size => {
+        loadFilter(sizeFilters, size, "size");
     })
 }
 loadFilters();
 
+const currentFilters = document.getElementById("current-filters");
+const filterItems = document.getElementById("filter-items")
+
+for(item of document.getElementsByClassName("filter-item")){
+
+    item.addEventListener("click", function(){
+
+        this.remove();
+        currentFilters.appendChild(this);
+        currentFilters.style.display = "flex";
+    })
+}
 searchInput.addEventListener('input', () =>{
     loadResults(searchInput.value, 1);
 })
