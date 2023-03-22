3f2009a28e0296a7346bb347a8a8ed6d62bd6d06
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-03-22 13:19:53 -0500

    working on code generation for the carts

--- js/cartManager.js
+++ js/cartManager.js
@@ -1,6 +1,6 @@
 class CartManager {
   //capturing all elements needed for the cart maanagement
-  static #cartState = GlobalStateManager.retrieve("cart");
+  static #cartState = () => GlobalStateManager.retrieve("cart");
   static #cart = document.getElementById("cart");
   static #cartAmount = document.getElementById("cart-amount");
 
@@ -24,12 +24,42 @@ class CartManager {
     }
   }
 
+  static findParentProdct(element) {
+    return element.closest(".product");
+  }
+
   //takes an element and generates a new cart item
-  static generateCartProduct(elemeent) {}
+  static generateCartProduct(product) {
+    let productImage = product.getElementsByClassName("product-image")[0];
+    let productPrice =
+      product.getElementsByClassName("product-price")[0].innerText;
+    let productId = product.dataset.productId;
+
+    return `
+      <div class="cart-product" data-id="${productId}">
+        <img src="${productImage.src}" alt="${productImage.alt}" />
+        <div class="cart-product-info">
+          <p class="product-price">${productPrice}</p>
+          <div class="product-quantity-changer">
+            <img src="../imgs/minus.svg" alt="minus" />
+            <p class="product-quantity">product price</p>
+            <img src="../imgs/plus.svg" alt="plus" />
+            <img src="../imgs/trash.svg" alt="remove" />
+          </div>
+        </div>
+      </div>
+    `;
+  }
 
   //adding product to cart
-  static addProductToCart(product) {}
+  static addProductToCart(product) {
+    CartManager.changeCartAmount(1);
+    CartManager.#cart.innerHTML += CartManager.generateCartProduct(product);
+  }
 
   //removing product from cart
-  static removeProductFromCart(product) {}
+  static removeProductFromCart(cartProduct) {
+    CartManager.changeCartAmount(-1);
+    cartProduct.remove();
+  }
 }
--- js/resultsManager.js
+++ js/resultsManager.js
@@ -1,37 +1,37 @@
-class ResultsManager{
+class ResultsManager {
+  //elements
+  element = document.getElementById("main-results");
+  msrpElement = document.getElementById("main-msrp");
 
-    //elements
-    element =  document.getElementById("main-results");
-    msrpElement = document.getElementById("main-msrp");
+  //result html manipulation
+  resetResults() {
+    this.element.innerHTML = "";
+  }
 
-    //result html manipulation
-    resetResults () {
-      this.element.innerHTML = "";
-    };
+  addResult(htmlElement) {
+    this.element.innerHTML += htmlElement;
+  }
 
-    addResult (htmlElement) {
-      this.element.innerHTML += htmlElement;
-    };
+  toHtml(result, sale = 0) {
+    let price = result.price;
+    let msrp = result.msrp;
+    let percentage = (100 - (price / msrp) * 100).toFixed(0);
 
-    toHtml (result, sale = 0) {
-      let price = result.price;
-      let msrp = result.msrp;
-      let percentage  =  (100 - ((price / msrp) * 100)).toFixed(0);
-      return ` <div
-        class="product"
-        data-id="${result.id}"
-        data-product-url="${result.url}"
-      >
+    return ` 
+      <div class="product" data-id="${result.id}" data-product-url="${
+      result.url
+    }">
         <div class="product-image-container">
           ${
-            this.msrpChecker(result)  
+            this.msrpChecker(result)
               ? `<span class="price-difference" data-msrp="${result.msrp}" data-price="${result.price}">-${percentage}%</span>`
               : ""
           }
           ${
-            (this.msrpChecker(result) && percentage >= 20) ?
-            `<img class="product-sale-icon" src="imgs/main/sale.png" />` : ""
-           }
+            this.msrpChecker(result) && percentage >= 20
+              ? `<img class="product-sale-icon" src="imgs/main/sale.png" />`
+              : ""
+          }
           <img class="product-image" src="${result.imageUrl}" />
         </div>
         <div class="product-information">
@@ -42,34 +42,34 @@ class ResultsManager{
           <img class="add-to-cart" src="imgs/main/add-to-cart.svg"/> 
         </div>
       </div>`;
-    };
+  }
 
-    //data extraction methods
+  //data extraction methods
 
-    //dont want to have to think about this later
-    //so i'm making sure the I can compare MSRP and PRICE
-    //easily
-    msrpChecker(result){
-      return result.msrp > result.price;
+  //dont want to have to think about this later
+  //so i'm making sure the I can compare MSRP and PRICE
+  //easily
+  msrpChecker(result) {
+    return result.msrp > result.price;
+  }
+  paginationData(result) {
+    return {
+      total: result.pagination.totalResults,
+      start: result.pagination.begin,
+      finish: result.pagination.totalPages,
+      current: result.pagination.currentPage,
     };
-    paginationData(result){
-      return {
-        total: result.pagination.totalResults,
-        start: result.pagination.begin,
-        finish: result.pagination.totalPages,
-        current: result.pagination.currentPage,
-      };
-    };
-    getName(result){
-      return result.name;
-    };
-    getPrice(result){
-      return result.price;
-    };
-    getImage(result){
-      return result.imageUrl;
-    };
-    getDescription(result){
-      return result.description;
-    };
-  };
+  }
+  getName(result) {
+    return result.name;
+  }
+  getPrice(result) {
+    return result.price;
+  }
+  getImage(result) {
+    return result.imageUrl;
+  }
+  getDescription(result) {
+    return result.description;
+  }
+}
