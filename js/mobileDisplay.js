fff8b76af8634c278680ec3b935b4eb587790c19
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-03-22 21:36:53 -0500

    autosave script

--- js/mobileDisplay.js
+++ js/mobileDisplay.js
@@ -1,14 +1,12 @@
-
 const modal = document.getElementById("search-and-cart-modal");
 const overlay = document.querySelector("#search-and-cart-modal .overlay");
 const content = document.querySelector("#search-and-cart-modal .content");
 const backButton = document.getElementById("search-back-button");
 
-backButton.addEventListener('click', () =>{
+backButton.addEventListener("click", () => {
   modal.style.width = 0;
-  content.style.width=0;
+  content.style.width = 0;
   overlay.style["background-color"] = "#0000";
-
 });
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
@@ -21,20 +19,20 @@ function hamburgerDisplay() {
   }
 }
 
-function autocomplete(){
-  
-}
-function searchAndCartDisplay(){
- 
-
+function autocomplete() {}
+function searchAndCartDisplay() {
   const isMobile = window.matchMedia("(max-width: 768px");
-
-  if(isMobile.matches){
-    console.log(`match: ${isMobile.matches}`)
+  if (isMobile.matches) {
+    console.log(`match: ${isMobile.matches}`);
     content.style.width = "80vw";
-  }else{
+  } else {
     content.style.width = "35vw";
   }
   modal.style.width = "100vw";
-  overlay.style["background-color"] = "#0009"; 
+  overlay.style["background-color"] = "#0009";
+}
+
+function startCart() {
+  CartManager.flipTo("cart");
+  searchAndCartDisplay();
 }
