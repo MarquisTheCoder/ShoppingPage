37abaf279bb5fb6565edf7bdf461fa7f2d363446
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-03-22 23:16:13 -0500

    autosave script

--- js/mobileDisplay.js
+++ js/mobileDisplay.js
@@ -12,10 +12,10 @@ backButton.addEventListener("click", () => {
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function hamburgerDisplay() {
   var x = document.getElementById("main-menu");
-  if (x.style.display === "block") {
+  if (x.style.display === "flex") {
     x.style.display = "none";
   } else {
-    x.style.display = "block";
+    x.style.display = "flex";
   }
 }
 
