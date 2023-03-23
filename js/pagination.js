94aca08b18c8e9e2111b6d3f0a6ab97428358e8d
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-03-22 23:28:21 -0500

    fixed pagination click back

--- js/pagination.js
+++ js/pagination.js
@@ -5,9 +5,23 @@ const pageUp = document.getElementById("page-up");
 const pageDown = document.getElementById("page-down");
 const currentPage = document.getElementById("pagination-current");
 
+setInterval(() => {
+  if (parseInt(currentPage.innerText) === 1) {
+    pageDown.classList.add("disabled");
+  } else if (
+    parseInt(currentPage.innerText) ===
+    GlobalStateManager.retrieve("totalPages")
+  ) {
+    pageUp.classList.add("disabled");
+  } else {
+    pageDown.classList.remove("disabled");
+    pageUp.classList.remove("disabled");
+  }
+}, 500);
 pageUp.addEventListener("click", () => {
   loadResults(parseInt(currentPage.innerText) + 1);
 });
+
 pageDown.addEventListener("click", () => {
   loadResults(parseInt(currentPage.innerText) - 1);
 });
